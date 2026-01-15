401332db131ef2062c9b192d8c702779b4740ca3
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2026-01-15 16:31:15 +0200

    fix(TraderJoeV2.2): remove inactive integration configs

--- src/dex/trader-joe-v2.1/config.ts
+++ src/dex/trader-joe-v2.1/config.ts
@@ -8,5 +8,4 @@ export const TRADERJOE_V2_1_ROUTER_ADDRESS: { [network: number]: Address } = {
 
 export const TRADERJOE_V2_2_ROUTER_ADDRESS: { [network: number]: Address } = {
   [Network.AVALANCHE]: '0x18556da13313f3532c54711497a8fedac273220e',
-  [Network.ARBITRUM]: '0x18556da13313f3532c54711497a8fedac273220e',
 };
