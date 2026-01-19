7263ac25a17e26904d3ec1001dd646176648bb8f
Danylo Kanievskyi <danylokaniev@gmail.com>
2026-01-19 16:54:33 +0200

    feat: increase min tvl to `$50k`

--- src/dex/algebra-integral/constants.ts
+++ src/dex/algebra-integral/constants.ts
@@ -3,7 +3,7 @@ export const ALGEBRA_GAS_COST = 180_000;
 export const ALGEBRA_EFFICIENCY_FACTOR = 3;
 
 // Minimum TVL in USD for a pool to be considered for pricing
-export const MIN_USD_TVL_FOR_PRICING = 10_000; // $10,000
+export const MIN_USD_TVL_FOR_PRICING = 50_000; // $50,000
 
 // TVL update interval in seconds
 export const POOL_TVL_UPDATE_INTERVAL = 5 * 60; // 5 minutes
